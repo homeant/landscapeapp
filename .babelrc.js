2d4de6a1f656be83bdb7c021e36497dd3edab932
Huang Tian Hui <huangtianhui@xiaobangtouzi.com>
2022-01-24 23:44:01 +0800

    add env

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,10 @@
 const path = require('path')
 
+const nextEnv = require("@next/env");
+
+
+nextEnv.loadEnvConfig(process.cwd());
+
 // CAREFUL before adding more presets, next/babel already includes some
 // see https://nextjs.org/docs/advanced-features/customizing-babel-config
 module.exports = {
